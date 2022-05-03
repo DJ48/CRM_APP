da7b2e206a42727ede52dd53e3b5a432ef1d9f9c
DJ48 <jaiswaldeepak264@gmail.com>
2022-05-04 00:17:39 +0530

    Adding Ticket Fetching and Updating Features

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -17,6 +17,7 @@ exports.createTicket = async(req,res) =>{
         title : req.body.title,
         description : req.body.description,
         ticketPriority : req.body.ticketPriority,
+        reporter : req.userId
     }
 
     /**
@@ -65,6 +66,109 @@ exports.createTicket = async(req,res) =>{
             message : "some internal error while craeting a new ticket"
         });
     }
+   
+}
 
+/**
+ *  API to fetch all the tickets
+ */
+
+exports.getAllTickets = async(req,res) =>{
+
+    const statusReq = req.query.status;
+    const queryObj = {
+        reporter : req.userId
+    }
+    if(statusReq){
+        queryObj.status = statusReq;
+    }
     
+    try{
+        const tickets = await Ticket.find(queryObj);
+        if(tickets.length == 0){
+            if(statusReq){
+                return res.status(201).send({
+                    message : "There is no "+statusReq+" ticket."
+                });
+            }
+            else{
+                return res.status(201).send({
+                    message : "You haven't created any ticket."
+                });
+            }
+        }
+        return res.status(201).send(objectConverter.ticketListResponse(tickets));
+
+    }catch(err){
+        console.error("Error while fetching all tickets: ", err.message);
+        return res.status(500).send({
+            message : "some internal error while fecthing all tickets"
+        });
+    }
+    
+}
+
+/**
+ *  Controller to fetch ticket based on id
+ */
+exports.getOneTicket = async(req,res) => {
+    
+    try {
+        const ticket = await Ticket.findOne({
+            _id : req.params.id
+        });
+        return res.status(201).send(objectConverter.ticketResponse(ticket));
+    } catch (err) {
+        console.error("Error while fetching ticket by ID: ", err.message);
+        return res.status(500).send({
+            message : "some internal error while fecthing ticket by ID"
+        });
+    }
+}
+
+/**
+ *  Controller to update the ticket
+ */
+
+exports.updateTicket = async(req,res)=>{
+
+    try {
+        // Check if the ticket exists
+        const ticket = await Ticket.findOne({
+            _id : req.params.id
+        });
+        console.log(ticket);
+        if(ticket == null){
+            return res.status(200).send({
+                message : "Ticket doesn't exist"
+            })
+        }
+
+        /**
+         *  Only the ticket requestor be allowed to update
+         */
+        if(ticket.reporter != req.userId){
+            return res.status(400).send({
+                message : "You are not authorized to update the ticket"
+            })
+        }
+
+        // Update the attributes of the saved ticket
+        ticket.title = req.body.title != undefined ? req.body.title : ticket.title;
+        ticket.description = req.body.description != undefined ? req.body.description : ticket.description;
+        ticket.ticketPriority = req.body.ticketPriority != undefined ? req.body.ticketPriority : ticket.ticketPriority;
+        ticket.status = req.body.status != undefined ? req.body.status : ticket.status;
+
+        // saved the changed ticket
+        const updatedTicket = await ticket.save();
+        
+        // Return the updated ticket
+
+        return res.status(200).send(objectConverter.ticketResponse(updatedTicket));
+    } catch (err) {
+        console.error("Error while updating ticket: ", err.message);
+        return res.status(500).send({
+            message : "some internal error while updating ticket"
+        });
+    }
 }
--- utils/objectConverter.js
+++ utils/objectConverter.js
@@ -31,3 +31,22 @@ exports.ticketResponse = (ticket)=>{
         updatedAt : ticket.updatedAt
     }
 }
+
+exports.ticketListResponse = (tickets) => {
+    ticketResult = [];
+    tickets.forEach(ticket => {
+        ticketResult.push({
+            title : ticket.title,
+            description : ticket.description,
+            ticketPriority : ticket.ticketPriority,
+            status : ticket.status,
+            reporter : ticket.reporter,
+            assignee : ticket.assignee,
+            id : ticket._id,
+            createdAt : ticket.createdAt,
+            updatedAt : ticket.updatedAt
+        })
+    })
+
+    return ticketResult;
+}
